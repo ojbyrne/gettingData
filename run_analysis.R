f736fa14a670b07e751a86c4f1bea3455c293c66
Owen Byrne <ojbyrne@gmail.com>
2014-09-21 15:39:19 -0700

    Mostly finished.

--- run_analysis.R
+++ run_analysis.R
@@ -17,7 +17,7 @@ names(train_y) <- c("Activity")
 names(test_subject) <- c("Subject")
 names(train_subject) <- c("Subject")
 
-# names for the data sets (test_x, train_x) with 561 columns
+# names for the data sets (test_x, train_x) with 561 columns, clean them up
 feature_names <- gsub("[\\(\\)]","", features[,2])
 feature_names <- gsub("Acc", "Accelerometer", feature_names) # expand some abbreviations
 feature_names <- gsub("Gyro", "Gyroscope", feature_names)
@@ -26,15 +26,16 @@ feature_names <- gsub("^t", "Time", feature_names) # change first letter "t" to 
 feature_names <- gsub("[,-]([a-zA-Z0-9])", "\\U\\1", feature_names, perl=TRUE) # remove dashes or commas, and capitalize the following letter
 names(test_x) <- feature_names
 names(train_x) <- feature_names
+
 train = cbind(train_x, train_y, train_subject)
 test = cbind(test_x, test_y, test_subject)
 full_data = rbind(train, test)
 full_data$Activity = activity_labels[full_data$Activity[],2] # convert the activity column to descriptive names
 mean_cols <- grep("mean", feature_names, ignore.case=TRUE)
 stdev_cols <- grep("std", feature_names, ignore.case=TRUE)
-means = cbind(full_data$Subject, full_data$Activity, full_data[, mean_cols])
-names(means)[0] = "Subject"
-names(means)[1] = "Activity"
-std = cbind(full_data$Subject, full_data$Activity, full_data[, stdev_cols])
-names(std)[0] = "Subject"
-names(std)[1] = "Activity"
+
+means_and_std = cbind(full_data$Subject, full_data$Activity, full_data[, mean_cols], full_data[,stdev_cols])
+tidy_data = aggregate(means_and_std[3:88], list(means[,1], means[,2]),mean)
+names(tidy_data)[1] = "Subject"
+names(tidy_data)[2] = "Activity"
+write.table(tidy_data, "tidy_data.txt", row_names=FALSE)
